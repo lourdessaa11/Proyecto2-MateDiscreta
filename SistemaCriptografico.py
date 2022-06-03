c109291a9fcb8e781883b14a03556cd3ecb0eb8c
lourdessaa11 <saa21333@uvg.edu.gt>
2022-06-03 16:44:43 -0600

    comentarios

--- SistemaCriptografico.py
+++ SistemaCriptografico.py
@@ -1,21 +1,22 @@
 #UVG - Mate Discreta 
 #Proyecto 2: Sistema Criptográfico RSA
+#Gabriela y Lourdes
 
 #Funciones de apoyo
-def calculoPhi(p,q):
+def calculoPhi(p,q):        #calcular phi
     phi = int((p-1)*(q-1))
     return phi
 
-def mod_inv(e,phi):
+def mod_inv(e,phi):         #calcular d: d*e congruente 1 mod(n)
     for d in range(1,phi):
         if((e%phi)*(d%phi) % phi==1):
             return d
 
-def llavePublica(p,q):
+def llavePublica(p,q):      #calcular n
     n= p*q
     return n
 
-def mcd(a,b):
+def mcd(a,b):               #mcd
     a,b = abs(a),abs(b)
     if a < b:
         a,b = b,a
@@ -24,7 +25,7 @@ def mcd(a,b):
         a,b = b,r 
     return a 
 
-def bloques(n):
+def bloques(n):             #calcular bloques del mensaje
     bloques=0
     lim="25"
     while int(lim)<n:
@@ -32,7 +33,7 @@ def bloques(n):
         lim+="25"
     return bloques
 
-def modpower(n,p,m):
+def modpower(n,p,m):       #exponenciacion modular
     if p==0:
         return 1
     if p%2==0:
@@ -40,7 +41,7 @@ def modpower(n,p,m):
     else:
         return (n*modpower(n,(p-1)/2,m)**2)%m
 
-def encriptar(mensaje,e,n,bloques):
+def encriptar(mensaje,e,n,bloques):     #encriptar el mensaje
     diccionario = { 'A':'00','B':'01','C':'02','D':'03','E':'04','F':'05','G':'06','H':'07','I':'08','J':'09','K':'10',
     'L':'11','M':'12','N':'13','O':'14','P':'15','Q':'16','R':'17','S':'18','T':'19','U':'20',
     'V':'21','W':'22','X':'23','Y':'24','Z':'25'}
@@ -58,7 +59,7 @@ def encriptar(mensaje,e,n,bloques):
         mensajeEncriptado += c + ' '
     return mensajeEncriptado
 
-def desencriptar(mensaje,d,n,bloques):
+def desencriptar(mensaje,d,n,bloques):  #desencriptar el mensaje
     abc = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     f_bloques = mensaje.split()
     resultado = ""
@@ -74,7 +75,6 @@ def desencriptar(mensaje,d,n,bloques):
 
 #Codificación de un mensaje
 #Input: M, p, q, e 
-
 def encriptación(p,q,e,mensaje):
     n = llavePublica(p,q)
     b = bloques(n)
@@ -84,7 +84,6 @@ def encriptación(p,q,e,mensaje):
 
 #Decodificación de un mensaje
 #Input C & (n,e)
-
 def desencriptacion(p,q,e,mensaje):
     phi = calculoPhi(p,q)
     d = mod_inv(e,phi)
